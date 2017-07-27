4901f9ea9d670257906f773f36a2ad1c4e27a316
abdullahchaudhry <abdullah.chaudhry@outlook.com>
2017-07-27 14:44:05 +0100

    Router should use pathname, not querystring relates #20"

--- public/dom.js
+++ public/dom.js
@@ -1,9 +1,23 @@
 (function() {
+  
+
   var button = document.getElementById("js-submit_button");
   button.addEventListener('click', function(e) {
     e.preventDefault();
     var genre = document.getElementById('js-genre_input').value;
     var year = document.getElementById('year').value;
+    // /search/?genre=action&year=2015
+    var url = '/search/?' + 'genre=' + genre + '&year=' + year;
+
+    // make the api call to server
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+
+      }
+    }
+    xhr.open("GET", url, true);
+    xhr.send();
     console.log(genre, year);
   });
 })();
--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
-
-
+const url = require('url');
 
 const router = (req, res) => {
-  const endpoint = req.url;
+  // make url object out of our url
+  const urlObj = url.parse(req.url, true);
+  const endpoint = urlObj.pathname;
+  console.log("req.url " + req.url);
   if (endpoint === '/') {
     const filePath = path.join(__dirname, "..", "public", "index.html");
     fs.readFile(filePath, (error, file) => {
@@ -22,9 +24,11 @@ const router = (req, res) => {
       }
     });
   } else if (endpoint === '/search') {
+    console.log("inside search");
     res.writeHead(200, {
       "Content-type": "text/html"
     });
+    // send api request
     res.end();
   } else {
     res.writeHead(404, {
