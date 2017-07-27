5f834d9bddc86118ce98bb8bda301a0fc9a9891c
Constantina <polyccon@gmail.com>
2017-07-27 14:51:55 +0100

    extract data from object from API call
    relates to #4#5

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-
+const apiCall = require("./selectmovie.js")
 
 
 const router = (req, res) => {
@@ -25,7 +25,18 @@ const router = (req, res) => {
     res.writeHead(200, {
       "Content-type": "text/html"
     });
-    res.end();
+    apiCall(function (body){
+      //var arr = Object.keys(body);
+      const respObject = JSON.parse(body);
+      const title = respObject["results"][0].title;
+      const overview = respObject["results"][0].overview;
+      const releaseDate = respObject["results"][0].release_date;
+      const poster = respObject["results"][0].poster_path;
+      
+      console.log(title);
+      res.end(title);
+  });
+
   } else {
     res.writeHead(404, {
       "Content-type": "text/html"
