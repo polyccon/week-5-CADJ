21f3cff0cffff72375e040d2f356965bafa70788
RogeredBacon <jinglis12@googlemail.com>
2017-07-27 17:38:30 +0100

    Added the URL builder for the API request and turned the response into a json #20 #4

--- public/dom.js
+++ public/dom.js
@@ -1,5 +1,5 @@
 (function() {
-  
+
 
   var button = document.getElementById("js-submit_button");
   button.addEventListener('click', function(e) {
@@ -7,7 +7,7 @@
     var genre = document.getElementById('js-genre_input').value;
     var year = document.getElementById('year').value;
     // /search/?genre=action&year=2015
-    var url = '/search/?' + 'genre=' + genre + '&year=' + year;
+    var url = '/search?' + 'genre=' + genre + '&year=' + year;
 
     // make the api call to server
     var xhr = new XMLHttpRequest();
@@ -18,6 +18,5 @@
     }
     xhr.open("GET", url, true);
     xhr.send();
-    console.log(genre, year);
   });
 })();
--- src/selectmovie.js
+++ src/selectmovie.js
@@ -1,9 +1,9 @@
 const request = require('request');
 const https = require("https");
 
-
-function makeServerRequest(callback) {
-var url = "http://api.themoviedb.org/3/discover/movie?api_key=5a4f97d81e4c916589c1286921cd7227&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=14&year=1957"
+function makeServerRequest(genre, year, callback) {
+  var url = "http://api.themoviedb.org/3/discover/movie?api_key=5a4f97d81e4c916589c1286921cd7227&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=";
+  url += genre + "&year=" + year;
   request(url, function(error, response, body) {
 
     if (error) {
@@ -14,7 +14,5 @@ var url = "http://api.themoviedb.org/3/discover/movie?api_key=5a4f97d81e4c916589
   });
 
 };
-
-//respObject["results"];
 
 module.exports = makeServerRequest;
