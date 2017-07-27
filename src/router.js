47fec336216d42dd4079b3134158892e966ec5b2
Constantina <polyccon@gmail.com>
2017-07-27 12:03:36 +0100

    router error handling and second test
    relates to #11#6

--- src/router.js
+++ src/router.js
@@ -1,27 +1,36 @@
 const fs = require('fs');
 const path = require('path');
 
+
+
 const router = (req, res) => {
   const endpoint = req.url;
   if (endpoint === '/') {
-    const filePath = path.join(__dirname, '..', 'public', 'index.html');
-    fs.readFile(filePath, (err, file) => {
-
-      if (err) {
+    const filePath = path.join(__dirname, "..", "public", "index.html");
+    fs.readFile(filePath, (error, file) => {
+      if (error) {
         res.writeHead(500, {
           "Content-type": "text/html"
         });
         res.end("<h1>So sorry, we've had a problem on our end.</h1>");
+
       } else {
         res.writeHead(200, {
           "Content-type": "text/html"
         });
         res.end();
       }
-      // }
-      // else if (endpoint === '/search') {
-      // }
     });
+  } else if (endpoint === '/search') {
+    res.writeHead(200, {
+      "Content-type": "text/html"
+    });
+    res.end();
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html"
+    });
+    res.end("<h1>Sorry, this page doesn't exist</h1>");
   }
 }
 
--- tests/router.test.js
+++ tests/router.test.js
@@ -12,5 +12,11 @@ tape('Home route', (t) => {
     t.equal(res.statusCode, 200, 'should respond with status code 200');
     t.end();
   })
-  
+});
+
+tape('Search route', (t) => {
+  shot.inject(router, { method: 'get', url: '/search' }, (res) => {
+    t.equal(res.statusCode, 200, 'should respond with status code 200');
+    t.end();
+  })
 });
