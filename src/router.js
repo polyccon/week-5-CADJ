c74bda3a7859f098dd983f78329370ff20b08bad
Constantina <polyccon@gmail.com>
2017-07-27 11:19:30 +0100

    code for home route
    relates to #16#6

--- src/router.js
+++ src/router.js
@@ -6,12 +6,23 @@ const router = (req, res) => {
   if (endpoint === '/') {
     const filePath = path.join(__dirname, '..', 'public', 'index.html');
     fs.readFile(filePath, (err, file) => {
+
+      if (err) {
+        res.writeHead(500, {
+          "Content-type": "text/html"
+        });
+        res.end("<h1>So sorry, we've had a problem on our end.</h1>");
+      } else {
+        res.writeHead(200, {
+          "Content-type": "text/html"
+        });
+        res.end();
+      }
+      // }
+      // else if (endpoint === '/search') {
+      // }
     });
-     
   }
-  else if (endpoint === '/search') {
-  }
-  
 }
 
 module.exports = router;
