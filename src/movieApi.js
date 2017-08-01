587f6d857353361f3c86c49f2225022219fee9bd
Constantina <polyccon@gmail.com>
2017-08-01 23:02:43 +0100

    added error for the case where api call is made with no key

--- src/movieApi.js
+++ src/movieApi.js
@@ -4,7 +4,7 @@ const apikey = process.env.API_KEY;
 
 function movieApi(genre, year, callback) {
   var url =
-  "http://api.themoviedb.org/3/discover/movie?api_key="+apikey+"&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=";
+    "http://api.themoviedb.org/3/discover/movie?api_key=" + apikey + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=";
   url += genre + "&year=" + year;
   request(url, function(error, response, body) {
 
